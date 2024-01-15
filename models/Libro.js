c3318ae0b18e2ee763cf6a6dc4ad4e504b2dcdb7
Tacjx <tomas.cabral.jauregui@gmail.com>
2024-01-15 12:41:05 -0300

    clase 6

--- models/Libro.js
+++ models/Libro.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/biblioteca", {
-useUnifiedTopology: true,
-useNewUrlParser: true,
-});
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+.then(()=>console.log("conectado a la database"))
+.catch(err => console.log("no se conecto"));
 const LibroSchema = new mongoose.Schema({
 titulo: String,
 autor: String
